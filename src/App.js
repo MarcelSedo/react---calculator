2297b0565c47bd8d2316f84a12f899282dc34c4f
MarcelSedo <marcel.sedo1@gmail.com>
2024-06-03 11:43:42 +0200

    zalozenie_react_projektu_zakl strukt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [value, setValue] = useState("")
+  return ( 
+  <div className="container">
+    <div className="calculator">
+      <form action="">
+        <div className="display">
+          <input type="text" />
+        </div>
+        <div>
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <input type="button" value="AC" />
+          <input type="button" value="DE" />
+          <input type="button" value="." />
+          <input type="button" value="/" />
+        </div>
+        <div>
+          <input type="button" value="7" />
+          <input type="button" value="8" />
+          <input type="button" value="9" />
+          <input type="button" value="*" />
+        </div>
+        <div>
+          <input type="button" value="4" />
+          <input type="button" value="5" />
+          <input type="button" value="6" />
+          <input type="button" value="+" />
+        </div>
+        <div>
+          <input type="button" value="1" />
+          <input type="button" value="2" />
+          <input type="button" value="3" />
+          <input type="button" value="-" />
+        </div>
+        <div>
+          <input type="button" value="00" />
+          <input type="button" value="0" />
+          <input type="button" value="=" />
+        </div>
+      </form>
+    </div>
     </div>
   );
 }
